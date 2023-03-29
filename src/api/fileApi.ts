a32120e89818334999d7e759d16aef9da60742ff
AdachiTetsuya <tetsuya_adachi@demia.co.jp>
2023-03-30 02:38:46 +0900

    API_BASE_URLを環境変数に

--- src/api/fileApi.ts
+++ src/api/fileApi.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://127.0.0.1:8000';
+const API_BASE_URL = process.env.REACT_APP_API_URL_DOMAIN;
 const fileApiEnd = `${API_BASE_URL}/async-app/video-file/`;
 
 export const getFileList = async () => {
